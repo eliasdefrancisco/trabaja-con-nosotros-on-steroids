db65e56a89edf720724f3cca76cf7fb9a333fe87
Elías de Francisco <eliasdefrancisco@gmail.com>
2023-05-25 20:55:17 +0200

    generate button functionality

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -3,20 +3,36 @@ import { promises as fs } from 'fs'
 import path from 'path'
 import ejs from 'ejs'
 
-export async function createHtml (companyId, questionsAndAnswers, companyName, description) {
+export async function createButtonHtml (companyId) {
   try {
-    const outputFileName = `${companyId}.html`
-    const templatePath = path.join(process.cwd(), 'public', 'template.ejs')
+    const outputFileName = `${companyId}-button.html`
+    const templatePath = path.join(process.cwd(), 'public', 'button.ejs')
     const outputPath = path.join(process.cwd(), 'public', outputFileName)
     const template = await fs.readFile(templatePath, 'utf-8')
-    console.log('!! api generate/route.js createHtml() companyName', companyName)
+    // Render the EJS template
+    const html = ejs.render(template, { companyId })
+    // Write the rendered HTML to a file in the /public folder
+    await fs.writeFile(outputPath, html)
+    return outputFileName
+  } catch (error) {
+    console.log('!! api generate/route.js createButton() error', error)
+    return undefined
+  }
+}
+
+export async function createFormHtml (companyId, questionsAndAnswers, companyName, description) {
+  try {
+    const outputFileName = `${companyId}-form.html`
+    const templatePath = path.join(process.cwd(), 'public', 'form.ejs')
+    const outputPath = path.join(process.cwd(), 'public', outputFileName)
+    const template = await fs.readFile(templatePath, 'utf-8')
     // Render the EJS template
     const html = ejs.render(template, { questionsAndAnswers, companyName, description })
     // Write the rendered HTML to a file in the /public folder
     await fs.writeFile(outputPath, html)
     return outputFileName
   } catch (error) {
-    console.log('!! api generate/route.js createHtml() error', error)
+    console.log('!! api generate/route.js createFormHtml() error', error)
     return undefined
   }
 }
@@ -29,13 +45,16 @@ export async function POST (request) {
   if (!companyId) {
     return new Response('No se ha especificado una descripción', { status: 400 })
   }
-  const generatedFileName = await createHtml(companyId, questionsAndAnswers, companyName, description)
-  console.log('!! api generate/route.js POST() generatedFileName', generatedFileName)
-  if (!generatedFileName) {
-    return new Response('No se ha podido generar el HTML', { status: 500 })
+  const generatedFormFileName = await createFormHtml(companyId, questionsAndAnswers, companyName, description)
+  if (!generatedFormFileName) {
+    return new Response('No se ha podido generar el HTML para el formulario', { status: 500 })
+  }
+  const generatedButtonFileName = await createButtonHtml(companyId)
+  if (!generatedButtonFileName) {
+    return new Response('No se ha podido generar el HTML para el boton', { status: 500 })
   }
   try {
-    return NextResponse.json(generatedFileName)
+    return NextResponse.json('html form and button generated')
   } catch {
     return new Response('No se ha podido transformar el JSON de GPT', { status: 500 })
   }
--- app/clientComponents/GenerateButton/index.js
+++ app/clientComponents/GenerateButton/index.js
@@ -1,13 +1,35 @@
 'use client'
 
 import { useGlobalContext } from '@/app/context/store'
+import { useState } from 'react'
 import styles from './styles.module.css'
 
 export default function GenerateButton () {
   const { companyId, gptData, companyName, description } = useGlobalContext()
+  const [htmlFromServer, setHtmlFromServer] = useState('')
+
+  const copyHtmlToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(htmlFromServer)
+    } catch (err) {
+      console.error('Error al copiar el texto al portapapeles: ', err)
+    }
+  }
+
+  async function downloadHtmlButton () {
+    return await fetch(`/${companyId}-button.html`)
+      .then(response => response.text())
+      .then(data => {
+        setHtmlFromServer(data)
+        console.log('html del boton descargado correctamente')
+      })
+      .catch(error => {
+        console.log('error al descargar el html del boton', error)
+      })
+  }
 
   async function handleGenerate () {
-    const generatedFileName = await fetch('/api/generate', {
+    const generationState = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -19,14 +41,24 @@ export default function GenerateButton () {
         description
       })
     })
-    console.log('!! GenerateButton generatedFileName', generatedFileName)
+    if (generationState.ok) {
+      console.log('html generado correctamente')
+      await downloadHtmlButton()
+    } else {
+      console.log('error al generar el html')
+    }
   }
 
   if (!companyId || !gptData.length) return null
 
   return (
-    <button className={styles.button} onClick={handleGenerate}>
-      Generar botón !!
-    </button>
+    <div className={styles.container}>
+      <button className={styles.button} onClick={handleGenerate}>
+        Generar botón !!
+      </button>
+      {htmlFromServer && <div dangerouslySetInnerHTML={{ __html: htmlFromServer }} />}
+      {htmlFromServer && <textarea className={styles.htmlCode} value={htmlFromServer} readOnly />}
+      {htmlFromServer && <button className={styles.copyCode} onClick={copyHtmlToClipboard}>Copiar código al portapapeles</button>}
+    </div>
   )
 }
